cc547c81c546525421535e54b565226c6af1c3dd
Yongming Fan <fanyo@149-160-240-245.dhcp-bl.indiana.edu>
2018-08-21 15:16:26 -0400

    add Paddle class

---
+++ src/project1/Paddle.java
@@ -0,0 +1,68 @@
+package project1;
+
+public class Paddle {
+
+	private int width;
+	private int height;
+	private int x;
+	private int y;
+	provate int speed;
+
+	public Paddle(int width, int height, int x, int y, int speed) {
+		super();
+		this.width = width;
+		this.height = height;
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+	}
+
+	public Paddle() {
+
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public getSpeed() {
+		return speed;
+	}
+
+	public setSpeed() {
+		this.speed = speed;
+	}
+
+	public void move() {
+                this.x = this.x + this.speed;
+        }
+
+}
