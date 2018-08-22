ff97fd6e92a24692dabe5684e306cf63ebc96955
kulsv <kulsv1393@gmail.com>
2018-08-21 23:57:22 -0400

    Add dimensions

--- src/com/dimension/Circle.java
+++ src/com/dimension/Circle.java
@@ -1,5 +1,26 @@
 package com.dimension;
 
 public class Circle {
+	private int radius;
+	private int centerX;
+	private int centerY;
 	
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public int getCenter_x() {
+		return centerX;
+	}
+	public void setCenter_x(int centerX) {
+		this.centerX = centerX;
+	}
+	public int getCenter_y() {
+		return centerY;
+	}
+	public void setCenter_y(int centerY) {
+		this.centerY = centerY;
+	}
 }
--- src/com/dimension/Rectangle.java
+++ src/com/dimension/Rectangle.java
@@ -1,5 +1,33 @@
 package com.dimension;
 
 public class Rectangle {
-
+	private int topX;
+	private int topY;
+	private int width;
+	private int height;
+	
+	public int getTopX() {
+		return topX;
+	}
+	public void setTopX(int topX) {
+		this.topX = topX;
+	}
+	public int getTopY() {
+		return topY;
+	}
+	public void setTopY(int topY) {
+		this.topY = topY;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
