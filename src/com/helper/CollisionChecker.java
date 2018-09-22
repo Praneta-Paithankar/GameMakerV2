6362ee0248f82a2d7386228ec09b3ef295b133df
cbrahme <42560156+cbrahme@users.noreply.github.com>
2018-09-22 19:28:49 -0400

    new file for Sprite collision

--- src/com/helper/CollisionChecker.java
+++ src/com/helper/CollisionChecker.java
@@ -3,8 +3,6 @@ package com.helper;
 import org.apache.log4j.Logger;
 
 import com.component.Ball;
-import com.component.CircularSprite;
-import com.component.SpriteElement;
 import com.dimension.Circle;
 import com.dimension.Coordinate;
 import com.dimension.Rectangle;
@@ -13,183 +11,86 @@ import com.infrastruture.Direction;
 
 public class CollisionChecker {
 	protected static Logger log = Logger.getLogger(CollisionChecker.class);
-	
-	public void checkCollisionOfSprites(SpriteElement sourceSprite) {
-		if(sourceSprite.getClass().getClass().toString().contains("CircularSprite")) {
-			checkCollisionBetweenBallAndWall(sourceSprite);
-		}else {
-			checkCollisionBetweenRectangleAndWall(sourceSprite);
-		}
-	}
-	
-	public void checkCollisionOfSprites(SpriteElement sourceSprite,SpriteElement destinationSprite) {
-
-		if(sourceSprite.getClass().getClass().toString().contains("CircularSprite") && destinationSprite.getClass().toString().contains("CircularSprite")) {
-			checkCollisionBetweenTwoCircles(sourceSprite, destinationSprite);
-		}
-		else if(sourceSprite.getClass().getClass().toString().contains("RectangularSprite") && destinationSprite.getClass().toString().contains("RectangularSprite")) {
-			checkCollisionBetweenTwoRectangles(sourceSprite, destinationSprite);
-		}
-		else if(sourceSprite.getClass().getClass().toString().contains("CircularSprite") && destinationSprite.getClass().toString().contains("RectangularSprite")) {
-			checkCollisionBetweenTwoRectangles(sourceSprite, destinationSprite);
-		}
-		else if(sourceSprite.getClass().getClass().toString().contains("RectangularSprite") && destinationSprite.getClass().toString().contains("CircularSprite")) {
-			checkCollisionBetweenTwoRectangles(sourceSprite, destinationSprite);
-		}
-		
-	}
-	
-	public  Direction checkCollisionBetweenBallAndWall(SpriteElement sprite) {
+	public  Direction checkCollisionBetweenBallAndWall(Ball ball) {
 		//Collision between ball and wall
-		int spriteX = sprite.getElementX();
-		int spriteY = sprite.getElementY();
-		int velX = sprite.getVelX();
-		int velY = sprite.getVelY();
+		
+		Circle circle = ball.getCircle();
+		Coordinate delta = ball.getDelta();
 		
  		//get current position of ball
- 		int left =  spriteX - sprite.getRadius();
- 		int right = spriteX + sprite.getRadius();
- 		int top = spriteY - sprite.getRadius();
- 		int bottom = spriteY + sprite.getRadius();
+ 		int left =  circle.getCenter().getX() - circle.getRadius();
+ 		int right = circle.getCenter().getX() + circle.getRadius();
+ 		int top = circle.getCenter().getY() - circle.getRadius();
+ 		int bottom = circle.getCenter().getY() + circle.getRadius();
  		
  		
- 		if((left <=0) && (velX < 0))
+ 		if((left <=0) && (delta.getX() < 0))
  		{
  		    return Direction.X;
  		}
- 		if((right >= Constants.BOARD_PANEL_WIDTH) && (velX > 0))
+ 		if((right >= Constants.BOARD_PANEL_WIDTH) && (delta.getX() > 0))
  		{
  			return Direction.X;
  		}
- 		if((top <=0) && (velY < 0))
+ 		if((top <=0) && (delta.getY() < 0))
  		{
  			return Direction.Y;
  		}
- 		if((bottom >= Constants.BOARD_PANEL_HEIGHT) && (velY > 0))
+ 		if((bottom >= Constants.BOARD_PANEL_HEIGHT) && (delta.getY() > 0))
  		{
  			return Direction.Y;
  		}
  	
 		return Direction.NONE;
-	}
-	
-	public Direction checkCollisionBetweenRectangleAndWall(SpriteElement sprite) {
-		int topLeftX = sprite.getElementX();
-		int topLeftY = sprite.getElementY();
-		
-		//collision with left wall
-		if(topLeftX == 0) {
-			return Direction.X;
-		}
-		//collision with right wall
-		if((topLeftX+sprite.getWidth()) == Constants.BOARD_PANEL_WIDTH) {
-			return Direction.X;
-		}
-		//collision with top wall
-		if(topLeftY == 0) {
-			return Direction.Y;
-		}
-		//collision with bottom wall
-		if((topLeftY+sprite.getHeight()) == Constants.BOARD_PANEL_HEIGHT) {
-			return Direction.Y;
-		}
-		
-		return Direction.NONE;
-	}
-	
-	public Direction checkCollisionBetweenTwoRectangles(SpriteElement sourceSprite, SpriteElement destinationSprite){
-		
-		// If one rectangle is on left side of other 
-		if (sourceSprite.getElementX() > (destinationSprite.getElementX()+destinationSprite.getWidth()) || 
-				destinationSprite.getElementX() > (sourceSprite.getElementX()+sourceSprite.getWidth())) 
-	        return Direction.NONE; 
-	  
-	    // If one rectangle is above other 
-		if (sourceSprite.getElementY() > (destinationSprite.getElementY()+destinationSprite.getHeight()) || 
-				destinationSprite.getElementY() > (sourceSprite.getElementY()+sourceSprite.getHeight())) 
-	        return Direction.NONE;
-	    
-		return Direction.X;
-		
-	}
-	
-	public Direction checkCollisionBetweenTwoCircles(SpriteElement sourceSprite,SpriteElement destinationSprite) {
-		
-		int sourceCircleX = sourceSprite.getElementX();
-		int sourceCircleY = sourceSprite.getElementY();
-		int destinationCircleX = destinationSprite.getElementX();
-		int destinationCircleY = destinationSprite.getElementY();
-		 
-		int changeX = sourceCircleX-destinationCircleX;
-		int changeY = sourceCircleY-destinationCircleY;
-		if (Math.sqrt((changeX)*(changeX) + (changeY)*(changeY)) <= (sourceSprite.getRadius()+destinationSprite.getRadius())){
-			if(sourceCircleX>destinationCircleX)
-				return Direction.X;
-			if(sourceCircleY>destinationCircleY)
-				return Direction.Y;
-		}
-		return Direction.NONE;
-		
+ 	
 	}
 
-	public Direction checkCollisionBetweenCircleAndRectangle(SpriteElement sourceSprite,SpriteElement destinationSprite)
+	public Direction checkCollisionBetweenCircleAndRectangle(Circle circle, Rectangle rectangle)
 	{
-		SpriteElement circleSprite;
-		SpriteElement rectangleSprite;
-	
-		if(sourceSprite.getClass().toString().contains("Circular")) {
-			circleSprite = sourceSprite;
-			rectangleSprite = destinationSprite;
-		}else {
-			circleSprite = destinationSprite;
-			rectangleSprite = sourceSprite;
-		}
 		
-		int circleX = circleSprite.getElementX();
-		int circleY = circleSprite.getElementY();
+		int centerX = circle.getCenter().getX();
+		int centerY = circle.getCenter().getY();
 		
-		float nearestX = Math.max(rectangleSprite.getElementX(), Math.min(circleX, rectangleSprite.getElementX() + rectangleSprite.getWidth()));
-		float nearestY = Math.max(rectangleSprite.getElementY(), Math.min(circleY, rectangleSprite.getElementY() + rectangleSprite.getHeight()));
+		float nearestX = Math.max(rectangle.getTopLeftCoordinate().getX(), Math.min(centerX, rectangle.getTopLeftCoordinate().getX() + rectangle.getWidth()));
+		float nearestY = Math.max(rectangle.getTopLeftCoordinate().getY(), Math.min(centerY, rectangle.getTopLeftCoordinate().getY() + rectangle.getHeight()));
 
-		int distance = (int) Math.sqrt(Math.pow(circleX-nearestX, 2) + Math.pow(circleY-nearestY, 2));
-		if(distance < circleSprite.getRadius())
+		int distance = (int) Math.sqrt(Math.pow(centerX-nearestX, 2) + Math.pow(centerY-nearestY, 2));
+		if(distance < circle.getRadius())
 		{			
-			int topRectangleX = rectangleSprite.getElementX();
+			int topRectangleX = rectangle.getTopLeftCoordinate().getX();
 		
-			int topLeftRectangleX = rectangleSprite.getElementX() + circleSprite.getRadius();
-			int topLeftRectangleY = rectangleSprite.getElementY() + circleSprite.getRadius();
-			int topRightRectangleX = rectangleSprite.getElementX() + rectangleSprite.getWidth() - circleSprite.getRadius();
-			int bottomLeftRectangleY = rectangleSprite.getElementY() + rectangleSprite.getHeight() - circleSprite.getRadius();
+			int topLeftRectangleX = rectangle.getTopLeftCoordinate().getX() + circle.getRadius();
+			int topLeftRectangleY = rectangle.getTopLeftCoordinate().getY() + circle.getRadius();
+			int topRightRectangleX = rectangle.getTopLeftCoordinate().getX() + rectangle.getWidth() - circle.getRadius();
+			int bottomLeftRectangleY = rectangle.getTopLeftCoordinate().getY() + rectangle.getHeight() - circle.getRadius();
 			
 			
-			if(circleX  <= topLeftRectangleX && circleY > topLeftRectangleY  && circleY < bottomLeftRectangleY ) {
+			if(centerX  <= topLeftRectangleX && centerY > topLeftRectangleY  && centerY < bottomLeftRectangleY ) {
 				// left side
 				return Direction.X;
-			}else if(circleX > topLeftRectangleX && circleX < topRightRectangleX && circleY<= topLeftRectangleY) {
+			}else if(centerX > topLeftRectangleX && centerX < topRightRectangleX && centerY<= topLeftRectangleY) {
 				//top
 				return Direction.Y;
-			}else if(circleX> topLeftRectangleX && circleX< topRightRectangleX && circleY>=bottomLeftRectangleY) {
+			}else if(centerX> topLeftRectangleX && centerX< topRightRectangleX && centerY>=bottomLeftRectangleY) {
 				//bottom
 				return Direction.Y;
-			}else if(circleX >= topRightRectangleX && circleY> topLeftRectangleY && circleY < bottomLeftRectangleY) {
+			}else if(centerX >= topRightRectangleX && centerY> topLeftRectangleY && centerY < bottomLeftRectangleY) {
 				//right
 				return Direction.X;
-			}else if( circleX <= topLeftRectangleX && circleY <= topLeftRectangleY && circleX >= (topRectangleX - circleSprite.getRadius())) {
+			}else if( centerX <= topLeftRectangleX && centerY <= topLeftRectangleY && centerX >= (topRectangleX - circle.getRadius())) {
 				// left top
 				return Direction.BOTH;
-			}else if(circleX <= topLeftRectangleX && circleY >= bottomLeftRectangleY) {
+			}else if(centerX <= topLeftRectangleX && centerY >= bottomLeftRectangleY) {
 				//left bottom
 				return Direction.BOTH;
-			}else if(circleX >= topRightRectangleX && circleY <= topLeftRectangleY) {
+			}else if(centerX >= topRightRectangleX && centerY <= topLeftRectangleY) {
 				// right top
 				return Direction.BOTH;
-			}else if (circleX >= topRightRectangleX && circleY >= bottomLeftRectangleY) {
+			}else if (centerX >= topRightRectangleX && centerY >= bottomLeftRectangleY) {
 				// right bottom
 				return Direction.BOTH;
 			}
 		}
 		return Direction.NONE;
 	}
-	
-	
 }
